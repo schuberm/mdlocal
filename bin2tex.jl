64b70b4127181ef292b4414839dd23892239f3e4
Samuel Huberman <samuel.huberman@gmail.com>
2014-09-10 21:32:37 -0400

    bin2tex.jl works for lammps-Feb14 binary output

---
+++ bin2tex.jl
@@ -0,0 +1,50 @@
+
+function readLammps(filename,numAtoms,streampos)
+	fid = open(filename,"r")
+	vel=Array(Float64,numAtoms,3)
+	seek(fid,streampos)
+		#Skip this many bytes in the header portion of each timestep
+		readbytes(fid,8)
+		readbytes(fid,8)
+		readbytes(fid,4)
+		readbytes(fid,4)
+		readbytes(fid,4)
+		readbytes(fid,4)
+		readbytes(fid,4)
+		readbytes(fid,4)
+		readbytes(fid,4)
+		readbytes(fid,8)
+		readbytes(fid,8)
+		readbytes(fid,8)
+		readbytes(fid,8)
+		readbytes(fid,8)
+		readbytes(fid,8)
+		#Care about these numbers
+		size_one=read(fid,Int32)
+		nchunk=read(fid,Int32)
+		atomindex=convert(Int32,0)
+		for i=1:nchunk
+				n=read(fid,Int32)
+				for j=1:convert(Int32,n/size_one)
+					for k=1:size_one
+						vel[atomindex+j,k]=read(fid,Float64)
+					end
+				end
+				atomindex=atomindex+convert(Int32,n/size_one)
+		end
+	streampos=position(fid)
+	close(fid)
+
+	return vel,streampos
+
+end
+
+filename="dump_1_1.bin"
+numTstep=0
+numAtoms=4096
+(vel,streampos)=readLammps(filename,numAtoms,numTstep)
+show(streampos)
+show(vel[1,:])
+(vel,streampos)=readLammps(filename,numAtoms,streampos)
+show(streampos)
+show(vel[1,:])
